0a07f177b606eebb4c6fcec4dcb745a4955d7a1b
bruno7591 <brunodejesus7591@gmail.com>
2023-02-15 12:22:43 +0100

    Update

--- Assets/PointManager.cs
+++ Assets/PointManager.cs
@@ -35,18 +35,18 @@ public class PointManager : MonoBehaviour
         ball.transform.position = new Vector3(1.409f, -0.953f, -5.114f);
         for (int i = 0; i < pins.Length; i++)
         {
-            pins[i].transform.rotation = Quaternion.Euler(new Vector3(0, 90, -90));
+            pins[i].transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
         }
-        pins[0].transform.position = new Vector3(-0.51f, 0f, 0f);
-        pins[1].transform.position = new Vector3(-1.073f, 0f, 0.6919f);
-        pins[2].transform.position = new Vector3(-1.071f, 0f, -0.004f);
-        pins[3].transform.position = new Vector3(-1.617f, 0f, -0.003f);
-        pins[4].transform.position = new Vector3(-0.2280f, 0f, 0.375f);
-        pins[5].transform.position = new Vector3(-0.763f, -0.069f, -2.863f);
-        pins[6].transform.position = new Vector3(-1.361f, 0f, 0.3699f);
-        pins[7].transform.position = new Vector3(-0.5270f, 0f, 0.7009f);
-        pins[8].transform.position = new Vector3(-0.7670f, 0f, 0.3919f);
-        pins[9].transform.position = new Vector3(0f, 0f, 0f);
+        pins[0].transform.position = new Vector3(-0.51f, 0.2f, 0f);
+        pins[1].transform.position = new Vector3(-1.073f, 0.2f, 0.6919f);
+        pins[2].transform.position = new Vector3(-1.071f, 0.2f, -0.004f);
+        pins[3].transform.position = new Vector3(-1.617f, 0.2f, -0.003f);
+        pins[4].transform.position = new Vector3(-0.2280f, 0.2f, 0.375f);
+        pins[5].transform.position = new Vector3(-0.763f, 0.2f, -2.863f);
+        pins[6].transform.position = new Vector3(-1.361f, 0.2f, 0.3699f);
+        pins[7].transform.position = new Vector3(-0.5270f, 0.2f, 0.7009f);
+        pins[8].transform.position = new Vector3(-0.7670f, 0.2f, 0.3919f);
+        pins[9].transform.position = new Vector3(0f, 0.2f, 0f);
         Score = 0;
         ScoreText.text = Score.ToString() + " PTS";
 
