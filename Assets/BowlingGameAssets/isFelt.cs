a7faa9f94b7983579b60add526d2a6b6e79df30c
dopage <55894959+dopage@users.noreply.github.com>
2023-02-21 21:05:29 +0100

    Remove errors in console

--- Assets/BowlingGameAssets/isFelt.cs
+++ Assets/BowlingGameAssets/isFelt.cs
@@ -7,36 +7,32 @@ public class isFelt : MonoBehaviour
     private float rotateX;
     private float rotateZ;
     public AudioClip clip;
-    public AudioSource audioSource;
+    //public AudioSource audioSource;
     public PointManager mg;
     private bool tombe = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = clip;
-
+        //audioSource = GetComponent<AudioSource>();
+        //audioSource.clip = clip;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Debug.Log(gameObject.transform.rotation);
+        //Debug.Log(gameObject.transform.rotation);
         rotateX = gameObject.transform.rotation.x;
         rotateZ = gameObject.transform.rotation.z;
         if (rotateX >= 0.6 || rotateX <= -0.6 || rotateZ >= 0.6 || rotateZ <= -0.6)
         {
-
             if (tombe == false)
             {
-
                 //gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
                 tombe = true;
                 mg.UpdateScore();
                 //mg.ReStart();
-                audioSource.Play();
+                //audioSource.Play();
             }
         }
     }
